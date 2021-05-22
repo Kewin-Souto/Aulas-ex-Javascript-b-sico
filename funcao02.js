16654cdf48bf6b5722bfefdc63edaa659f38bf8a
Kewin-Souto <kevinstbarros@gmail.com>
2021-05-22 14:51:42 -0300

    ex 014 e 015

--- funcao02.js
+++
@@ -1,5 +0,0 @@
-function soma (n1=0, n2=0) { // Você atribuindo o valor zero neste parametro garante que no método abaixo no caso o log(soma(7)) recebe apenas um número na conta 
-    return n1 = n2
-}
-
-console.log(soma(7))
